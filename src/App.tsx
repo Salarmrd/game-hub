965ee535d6df4c64c08d7bee8babeb28bd6c641c
Amir <amirsalarmehrdad22@gmail.com>
2024-07-05 16:57:47 +0300

    Navbar with color switch added

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,27 @@
-import { Button, ButtonGroup } from "@chakra-ui/react";
+import { Grid, GridItem, Show } from "@chakra-ui/react";
+import Navbar from "./component/Navbar";
+
 function App() {
   return (
     <>
-      <Button colorScheme="blue">Button</Button>
+      <Grid
+        templateAreas={{
+          base: `"nav" "main"`,
+          lg: `"nav nav" "aside main"`,
+        }}
+      >
+        <GridItem area="nav">
+          <Navbar />
+        </GridItem>
+        <Show above="lg">
+          <GridItem area="aside" bg="yellow">
+            Aside
+          </GridItem>
+        </Show>
+        <GridItem area="main" bg="blue">
+          Main
+        </GridItem>
+      </Grid>
     </>
   );
 }
